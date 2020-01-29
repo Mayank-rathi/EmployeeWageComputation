33b7bbb7b0c09ae0c55436a97e05b42f83fc3082
Mayank-rathi <mayankrathi33@gmai.com>
2020-01-29 15:12:06 +0530

    ADDED:For loop and TotalSalary

--- employeeWageComputation.sh
+++ employeeWageComputation.sh
@@ -5,11 +5,15 @@ echo " welcome Employee Wage Computation "
 IS_PART_TIME=1;
 IS_FULL_TIME=2;
 EMPLOYEE_RATE_PER_HOUR=20;
+WORKING_DAYS_PER_MONTH=20;
 
 #VARIABLE
-employeeCheck=$((RANDOM%3))
+totakSalary=0;
 
-case $employeeCheck in
+for((day=1; day<=$WORKING_DAYS_PER_MONTH; day++))
+do
+		employeeCheck=$((RANDOM%3))
+	case $employeeCheck in
 		$IS_FULL_TIME )
 				employeeHours=8
 				;;
@@ -20,5 +24,6 @@ case $employeeCheck in
 				employeeHours=0
 				;;
 esac
-
-salary=$((employeeHours*EMPLOYEE_RATE_PER_HOUR));
+	salary=$((employeeHours*EMPLOYEE_RATE_PER_HOUR));
+	totalSalary=$(($totalSalary + $salary));
+done
